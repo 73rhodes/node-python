e68312a3bc3f05ed224c4bc2cd3e77b9ce8ed6c9
Darren DeRidder <drderidder@gmail.com>
2011-07-19 18:10:40 -0400

    Replaced stderr-based command delimiter with a rudimentary stdout one

--- test/python.test.js
+++ test/python.test.js
@@ -1,12 +1,22 @@
 var assert = require('assert');
 var python = new (require('../lib/python').Python);
 
-var testPrint = function() {
+var runTests = function() {
+   // Run a couple commands in series
    python.cmd('print "Hello World!"', function(err, data) {
       assert.equal('Hello World!\n', data);
-      console.log('testPrint: ok!');
-      python.cmd('quit()');
+      console.log('test 1 ok!');
+      python.cmd('print "Goodbye, Cruel World!"', function (err, data) {
+        assert.equal('Goodbye, Cruel World!\n', data);
+        console.log('test 2 ok!');
+        python.cmd('quit()');
+      });
+   });
+   // Run one in parallel with the first two
+   python.cmd('print "Asynch"', function (err, data) {
+     assert.equal('Asynch\n', data);
+     console.log('test 3 ok!');
    });
 };
 
-testPrint();
+runTests();
